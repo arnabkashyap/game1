5b48a9ca557009c9a6ed029fb36ed24040d0dec2
arnabkashyap <arnabkumarkashyap2006@gmail.com>
2026-05-11 21:45:59 +0530

    Added Car Customization (Garage) and Online Multiplayer support

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+let players = {};
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on('move', (data) => {
+        players[socket.id] = data;
+        io.emit('updatePlayers', players);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+        delete players[socket.id];
+        io.emit('playerDisconnected', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
